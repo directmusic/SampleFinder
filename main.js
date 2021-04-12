88394d1e97bebd16a7f79f322fa291ed863185d1
Joseph Lyncheski <directmusic94@gmail.com>
2021-04-11 23:18:55 -0400

    updated to have sorting

--- main.js
+++ main.js
@@ -60,6 +60,11 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// LAST RESULT LIST
+var results = "";
+var files = [];
+
 function getfilename(str) {
   let temp = str.split('/');
   return temp[temp.length - 1];
@@ -69,57 +74,159 @@ function tokenizefilters(str) {
 }
 
 function check_sample_validity(element) {
-  if (element.toLowerCase().indexOf("asd") >= 0 ||
-      element.toLowerCase().indexOf("reapeaks") >= 0) {
+  if (element.path.toLowerCase().indexOf("asd") >= 0 ||
+      element.path.toLowerCase().indexOf("reapeaks") >= 0) {
     return false;
   }
-  if (element.toLowerCase().indexOf("wav") >= 0 ||
-      element.toLowerCase().indexOf("aif") >= 0 ||
-      element.toLowerCase().indexOf("mp3") >= 0 ||
-      element.toLowerCase().indexOf("m4a") >= 0){
+  if (element.path.toLowerCase().indexOf("wav") >= 0 ||
+      element.path.toLowerCase().indexOf("aif") >= 0 ||
+      element.path.toLowerCase().indexOf("mp3") >= 0 ||
+      element.path.toLowerCase().indexOf("m4a") >= 0){
     return true;
     
   }
 }
-
-async function fromDir(startPath, filter){
-  var results = "";
-  var filter_tokens = tokenizefilters(filter);
-  var files = [];
-  for (let i = 0; i < startPath.length; i++){
-    let temp = fg.sync(startPath[i] + "**", { onlyFiles: true , globstar: true });
-    temp.forEach(element => {
+function clean_sample_list(list) {
+      list.forEach(element => {
       if (check_sample_validity(element)) {
         files.push(element);
       }
     });
-    
+}
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+
+  return array;
+}
+function sort_files(files, sort_mode) {
+    if (sort_mode == 1) {
+    files.sort(function(a, b) {
+      // return a.stats.ctimeMs < b.stats.ctimeMs;
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sort_mode == 2) {
+    files.sort(function(a, b) {
+      // return  < b.stats.ctimeMs;
+      var timeA = a.stats.ctimeMs; // ignore upper and lowercase
+      var timeB = b.stats.ctimeMs; // ignore upper and lowercase
+      if (timeA > timeB) {
+        return -1;
+      }
+      if (timeA < timeB) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sort_mode == 3) {
+    shuffle(files);
+  }
+}
+function update_results(file) {
+  results += "<a class='sample' href='javascript:void(0);' onmouseout='hoveroff()' onclick='hover(\"" + file.path + "\")' onmouseover='hover(\"" + file.path + "\")' ondrag='dragfile(\"" + file.path + "\")'>"+ file.name + "</a> <br>";
+}
+
+async function from_search(startPath, filter, sort_mode){
+  results = "";
+  files = [];
+  let temp_files = [];
+
+  var filter_tokens = tokenizefilters(filter);
+
+  for (let i = 0; i < startPath.length; i++){
+    let temp = fg.sync(startPath[i] + "**", { onlyFiles: true , globstar: true, stats: true});
+    clean_sample_list(temp);
+  }
+  sort_files(files, sort_mode);
+
   for(var i=0;i<files.length;i++){
-    files[i] = files[i].replaceAll(/'/g,"&apos;")
-    files[i] = files[i].replaceAll(/"/g,"&quot;")
+    files[i].path = files[i].path.replaceAll(/'/g,"&apos;")
+    files[i].path = files[i].path.replaceAll(/"/g,"&quot;")
 
     // takes the split tokens and checks if we match all of them.
     let n_valid_tokens = 0;
     for(var j = 0; j < filter_tokens.length; j++){
-      if (files[i].toLowerCase().indexOf(filter_tokens[j]) >= 0) {
+      if (files[i].path.toLowerCase().indexOf(filter_tokens[j]) >= 0) {
         n_valid_tokens++;
       }
       if (n_valid_tokens == filter_tokens.length){
-        results += "<a class='sample' href='javascript:void(0);' onmouseout='hoveroff()' onclick='hover(\"" + files[i] + "\")' onmouseover='hover(\"" + files[i] + "\")' ondrag='dragfile(\"" + files[i] + "\")'>"+ getfilename(files[i]) + "</a> <br>";
+        temp_files.push(files[i]);
       }
     }
   }
+  for(let i = 0; i < temp_files.length; i++){
+    update_results(temp_files[i]);
+  }
+  files = temp_files;
   return results;
 };
 
-ipcMain.on('search', (event, args) => {
+async function from_folder(startPath, sort_mode){
+  results = "";
+  files = [];
+
+  let temp = fg.sync(startPath + "**", { onlyFiles: true , globstar: true, stats: true});
+  clean_sample_list(temp);
+  sort_files(files, sort_mode);
+
+  for(var i=0;i<files.length;i++){
+    files[i].path = files[i].path.replaceAll(/'/g,"&apos;")
+    files[i].path = files[i].path.replaceAll(/"/g,"&quot;")
+    update_results(files[i]);
+  }
+  return results;
+};
+async function change_sort(sort_mode){
+  results = "";
+  sort_files(files, sort_mode);
+  for(var i = 0 ; i < files.length; i++){
+    update_results(files[i]);
+  }
+  return results;
+}
+
+ipcMain.on('search', (event, args, sort_mode) => {
   event.sender.send('change_reply_content', "<div id='loading'>Loading...</div>")
 
-  fromDir(settings_json.search_paths, args.toLowerCase()).then(
+  from_search(settings_json.search_paths, args.toLowerCase(), sort_mode).then(
     function(value) {event.sender.send('change_reply_content', value)}
   );
- // win.setSize(700, 1000, true);
+});
+
+ipcMain.on('folder', (event, args, sort_mode) => {
+  event.sender.send('change_reply_content', "<div id='loading'>Loading...</div>")
+
+  from_folder(args, sort_mode).then(
+    function(value) {event.sender.send('change_reply_content', value)}
+  );
+});
+
+ipcMain.on('changesort', (event, sort_mode) => {
+  event.sender.send('change_reply_content', "<div id='loading'>Loading...</div>")
+
+  change_sort(sort_mode).then(
+    function(value) {event.sender.send('change_reply_content', value)}
+  );
 });
 
 ipcMain.on('favorites', (event, args) => {
@@ -131,30 +238,6 @@ ipcMain.on('favorites', (event, args) => {
   event.sender.send('change_reply_content', temp)
 });
 
-async function load_all_files_in_folder(startPath){
-  var results = "";
-  var files = [];
-
-  let temp = fg.sync(startPath + "**", { onlyFiles: true , globstar: true });
-  temp.forEach(element => {
-    if (check_sample_validity(element)) {
-      files.push(element);
-    }
-  });
-    
-  for(var i=0;i<files.length;i++){
-    results += "<a class='sample' href='javascript:void(0);' onmouseout='hoveroff()' onclick='hover(\"" + files[i] + "\")' onmouseover='hover(\"" + files[i] + "\")' ondrag='dragfile(\"" + files[i] + "\")'>"+ getfilename(files[i]) + "</a> <br>";
-  }
-  return results;
-};
-
-ipcMain.on('folder', (event, args) => {
-  event.sender.send('change_reply_content', "<div id='loading'>Loading...</div>")
-
-  load_all_files_in_folder(args).then(
-    function(value) {event.sender.send('change_reply_content', value)}
-  );
-});
 
 ipcMain.on('ondragstart', (event, filePath) => {
   event.sender.startDrag({
--- renderer.js
+++ renderer.js
@@ -1,18 +1,24 @@
 const ipc = require('electron').ipcRenderer;
 
+let sort_mode = 0;
+
 const submit_button = document.querySelector('#form');
 
 const file_name = document.querySelector('#file_name');
 let reply_div = document.querySelector('#reply');
 
 submit_button.addEventListener('submit', () => {
- ipc.send('search', file_name.value)
+ ipc.send('search', file_name.value, sort_mode);
 });
 
 ipc.on('change_reply_content', (event, args) => {
  reply_div.innerHTML = args;
 });
 
+function sort_by(sort) {
+	sort_mode = sort;
+	ipc.send('changesort', sort);
+}
 function dragfile(url) {
     ipc.send('ondragstart', url)
 }
@@ -20,7 +26,7 @@ function open_favorites() {
 	ipc.send('favorites', 0)
 }
 function open_folder(url) {
-	ipc.send('folder', url)
+	ipc.send('folder', url, sort_mode)
 }
 function open_settings(url) {
 	ipc.send('settings', url)
